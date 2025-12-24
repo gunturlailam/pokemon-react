6c815dfa0f799c2ff781d02dda49ec1682046381
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-12-24 22:29:59 +0700

    search - end projek

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -6,15 +6,26 @@ import PokemonItem from '../PokemonItem/PokemonItem';
 
 function PokemonList() {
     const [pokemons] = useState(pokemonJSON);
+    const [filterPokemons, setFilterPokemons] = useState(pokemonJSON);
+    const handleSearch = (e) => {
+        let search = pokemons.filter((item) =>{
+            return item.name.toLowerCase().includes(e.target.value.toLowerCase());
+        });
+        setFilterPokemons(search);
+    }
 
     return(
         <div>
+            <input type="text" placeholder="Cari Pokemon..." className="search" onChange={handleSearch} />
+
             <div className='list-pokemon'>
-                {
-                    pokemons.map((item) => (
+            {filterPokemons.length === 0 ? (
+                <div className='not-found'>Pokemon tidak ditemukan</div>
+                ) : (
+                    filterPokemons.map((item) => (
                     <PokemonItem key={item.id} pokemon={item} />    
-                    ))
-                }
+                ))
+            )}
             </div>
         </div>
     )
